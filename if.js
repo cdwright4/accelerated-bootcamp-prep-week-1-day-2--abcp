c7af750127056604fd68bf8508716b2111f7f67b
Crystal <crystal_wright@yahoo.com>
2018-01-14 18:45:45 +0000

    automatically backed up by learn

--- if.js
+++ if.js
@@ -4,5 +4,13 @@ if (pokemon == "Pikachu") {
   console.log("I choose you Pikachu!!")
 }
 
-
+function newFunction (num1) {
+  
+  if (num1 > 10){
+    return num1;
+  }
+  else {
+    return "Sorry"!;
+  }
+}
 
