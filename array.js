b29cd2cec7729b88a3443f82592a13fe2f22f782
Crystal <crystal_wright@yahoo.com>
2018-01-14 18:11:03 +0000

    automatically backed up by learn

--- array.js
+++ array.js
@@ -37,18 +37,25 @@ function accessElementInArray(array, index) {
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
   
-  array.pop();
+  array.shift();
   return array;
 
 }
 
 function removeElementFromBeginningOfArray(array) {
+  return array.slice(1);
+  
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
+  
+  array.pop();
+  return array;
 
 }
 
 function removeElementFromEndOfArray(array) {
+  
+  array.slice(0,array.length);
 
 }
