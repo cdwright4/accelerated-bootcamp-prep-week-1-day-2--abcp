bf4e50453d9b73e550affd09d67f713d4f27c686
Crystal <crystal_wright@yahoo.com>
2018-01-14 17:37:39 +0000

    automatically backed up by learn

--- array.js
+++ array.js
@@ -1,7 +1,7 @@
 function addElementToBeginningOfArray(array, element) {
  // add the element to the beginning of the array without mutating the array
  
- 
+ var newArray = array.unshift(element);
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
